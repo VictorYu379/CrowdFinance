059331f6278adf6ad08512f5cba36c942df5a9ef
Victor Yu <victoryu379@gmail.com>
2024-12-07 13:37:20 -0500

    redirect to mobile version when visiting from mobile

---
+++ mobile-a/script.js
@@ -0,0 +1,9 @@
+// Initialize Swiper.js
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
