72ba5f77dcf0f8ddea839182bd2cead609934811
Victor Yu <victoryu379@gmail.com>
2024-12-07 13:58:19 -0500

    fixed mobile version

--- mobile-a/script.js
+++ mobile-a/script.js
@@ -1,6 +1,6 @@
 // Initialize Swiper.js
 const swiper = new Swiper('.swiper-container', {
-    slidesPerView: 1,
+    slidesPerView: 1.1,
     spaceBetween: 10,
     pagination: {
         el: '.swiper-pagination',
